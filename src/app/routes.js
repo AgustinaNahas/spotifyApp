68e3bd53a1181de957ff56d87d8ef60f7618e774
María Agustina Nahas <maria.nahas@globant.com>
2016-11-22 14:29:47 -0300

    Controllers agregados a los routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,4 +1,8 @@
 import 'angular-route';
+import {favCtrl} from './controllers';
+import {artistCtrl} from './controllers';
+import {albumCtrl} from './controllers';
+import {searchCtrl} from './controllers';
 
 export function routes($routeProvider, $locationProvider) {
     $locationProvider.html5Mode({
@@ -9,15 +13,19 @@ export function routes($routeProvider, $locationProvider) {
 
     $routeProvider
     .when('/', {
-        templateUrl : '/app/views/index.html'
+        templateUrl : '/app/views/index.html',
+        controller: favCtrl
     })
     .when('/artist/:id', {
-        templateUrl : '/app/views/band-albums.html'
+        templateUrl : '/app/views/band-albums.html',
+        controller: artistCtrl
     })
     .when('/results', {
-        templateUrl : '/app/views/results.html'
+        templateUrl : '/app/views/results.html',
+        controller: searchCtrl
     })
     .when('/album/:id', {
-        templateUrl : '/app/views/album-detail.html'
+        templateUrl : '/app/views/album-detail.html',
+        controller: albumCtrl
     });
 }
